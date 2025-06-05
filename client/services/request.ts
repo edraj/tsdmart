668856975c36d9975a7aa0f25b8d00535cf674b9
ayyash <abdul_ayyash@yahoo.com>
2025-06-05 14:17:41 +0300

    save

--- client/services/request.ts
+++ client/services/request.ts
@@ -13,7 +13,7 @@ export const request = async (
 };
 
 // this is useless
-export const submit = async (
+export const submit_old = async (
   client: AxiosInstance,
   spaceName: string,
   schemaShortname: string,
@@ -23,7 +23,36 @@ export const submit = async (
   const { data } = await client.post(
     `public/submit/${spaceName}/${schemaShortname}/${subpath}`,
     record,
-    { headers: Config.headers }
+    { headers: Config.client.headers }
   );
   return data;
+};
+
+
+export const submit = async (
+  spaceName: string,
+  schemaShortname: string,
+  subpath: string,
+  record: any,
+  resourceType?: string,
+  workflowShortname?: string,
+) => {
+  try {
+    var url = `public/submit/${spaceName}`;
+    if (resourceType) {
+      url += `/${resourceType}`;
+    }
+    if (workflowShortname) {
+      url += `/${workflowShortname}`;
+    }
+    url += `/${schemaShortname}/${subpath}`;
+    const { data } = await axios.post(
+      url,
+      record,
+      { headers }
+    );
+    return data;
+  } catch (error: any) {
+    throw error;
+  }
 };
